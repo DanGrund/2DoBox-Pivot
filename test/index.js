06868ccec405e45d3d13b5311b7eb43590ec2ca0
JeffBuss <turbocapslock@gmail.com>
2016-11-29 14:07:59 -0700

    Add initial tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,42 @@
-const assert = require('chai').assert
+/*jshint esversion: 6 */
 
-describe('our test bundle', function () {
-  it('should work', function () {
-    assert(true)
-    })
+const assert = require('chai').assert;
+
+
+describe('our test bundle', () => {
+  context('with default attributes', () => {
+
+    it('should work', () => {
+      assert(true);
+    });
+
+    it('should have an empty idea array by default ', () => {
+      assert.isArray(ideaArray);
+    });
+
+    it('should have an empty sorted array by default', () => {
+      assert(true);
+    });
+
+    it('should have a sort order of false by default', () => {
+      assert(true);
+    });
+
+    it('if ideas exist in local storage they are rendered to the page', () => {
+      assert(true);
+    });
+
+    it('should store ideas in local storage', () => {
+      assert(true);
+    });
+
+    it('should delete ideas from local storage', () => {
+      assert(true);
+    });
   });
+});
+//
+// DOM tests
+// save button creates new card
+// card persists on refresh
+// delete button does shit
